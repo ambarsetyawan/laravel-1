5ea36de1346089b42815557afe46d49ca0497e84
Tran-Moi-Mulodo <tran.moi@mulodo.com>
2015-05-19 17:12:45 +0700

    Format code

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -1,5 +1,6 @@
-<?php namespace App\Http\Controllers;
+<?php
 
+namespace App\Http\Controllers;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
@@ -10,25 +11,27 @@ use Redirect;
 use Validator;
 use Auth;
 use Session;
+
 class PostController extends Controller {
 
 	/**
 	 * Create a new controller instance.
-	 *
+	 * 
 	 * @return void
 	 */
-	public function __construct()
+	public function __construct ()
 	{
 		$this->middleware('auth');
 	}
 
 	/**
 	 * Get create post
+	 * 
 	 * @author Tran Van Moi
 	 * @since 2015/05/19
-	 * @return  response
+	 * @return response
 	 */
-	public function getCreate()
+	public function getCreate ()
 	{
 		$data['categories'] = Category::all();
 		return view('post.create', $data);
@@ -36,126 +39,137 @@ class PostController extends Controller {
 
 	/**
 	 * post create post
+	 * 
 	 * @author Tran Van Moi
 	 * @since 2015/05/19
 	 * @return response
 	 */
-	public function postCreate()
+	public function postCreate ()
 	{
 		$data = Input::all();
 		$validator = Post::validate($data);
-		if($validator->fails()){
+		if ($validator->fails()) {
 			return Redirect::to('post/create')->withInput()->withErrors($validator);
-		}
-		else{
+		} else {
 			$post = Post::create_post($data);
-			if(count($post))
-				return redirect('post/create')->withInput()->with('post_status', ['status' => 'success', 'message' => 'Create a new post is success!']);
+			if (count($post))
+				return redirect('post/create')->withInput()->with('post_status', 
+						[
+								'status' => 'success',
+								'message' => 'Create a new post is success!']);
 			else
-				return redirect('post/create')->withInput()->with('post_status', ['status' => 'danger', 'message' => 'Create a new post is failed! Please try again!']);
+				return redirect('post/create')->withInput()->with('post_status', 
+						[
+								'status' => 'danger',
+								'message' => 'Create a new post is failed! Please try again!']);
 		}
 	}
 
 	/**
 	 * Display the specified resource.
-	 *
-	 * @param  int  $id
+	 * 
+	 * @param int $id        	
 	 * @return Response
 	 */
-	public function show($id)
+	public function show ($id)
 	{
 		//
 	}
 
 	/**
 	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
+	 * 
+	 * @param int $id        	
 	 * @return Response
 	 */
-	public function getEdit($post_id = null)
+	public function getEdit ($post_id = null)
 	{
-		$data = ['post_id' => $post_id];
-		$rules = ['post_id' => 'required|exists:posts,id'];
-		$validator = Validator::make($data, $rules);			
-		if($validator->fails()){
-			Session::flash('post_status', ['status' => 'danger', 'message' => 'Post is not found!']);
-		}
-		else {
+		$data = [
+				'post_id' => $post_id];
+		$rules = [
+				'post_id' => 'required|exists:posts,id'];
+		$validator = Validator::make($data, $rules);
+		if ($validator->fails()) {
+			Session::flash('post_status', [
+					'status' => 'danger',
+					'message' => 'Post is not found!']);
+		} else {
 			$check_role = Post::whereUser_id(Auth::user()->id)->whereId($data['post_id'])->first();
 			$check_delete_post = Post::whereDelete_status(0)->whereId($data['post_id'])->first();
-			if($check_role && $check_delete_post){
-				$data['post'] = $check_delete_post;				
-			}
-			else
-				Session::flash('post_status', ['status' => 'danger', 'message' => 'You dont have permission to edit this post!']);
+			if ($check_role && $check_delete_post) {
+				$data['post'] = $check_delete_post;
+			} else
+				Session::flash('post_status', 
+						[
+								'status' => 'danger',
+								'message' => 'You dont have permission to edit this post!']);
 		}
 		$data['categories'] = Category::all();
 		return view('post/edit', $data);
 	}
+
 	/**
 	 * Show the form for editing the specified resource.
-	 *
-	 * @param  int  $id
+	 * 
+	 * @param int $id        	
 	 * @return Response
 	 */
-	public function postEdit($post_id = null)
+	public function postEdit ($post_id = null)
 	{
 		$check_delete_post = Post::whereDelete_status(0)->whereId($post_id)->first();
-		if($check_delete_post){
+		if ($check_delete_post) {
 			$data = Input::all();
 			$validator = Post::validate($data);
-			if($validator->fails()){
-				return Redirect::to('post/edit/'.$post_id)->withInput()->withErrors($validator);
-			}
-			else {
+			if ($validator->fails()) {
+				return Redirect::to('post/edit/' . $post_id)->withInput()->withErrors($validator);
+			} else {
 				$check_delete_post->title = $data['title'];
 				$check_delete_post->content = $data['content'];
 				$check_delete_post->category_id = $data['category'];
 				$check_delete_post->save();
-				Session::flash('post_status', ['status' => 'success', 'message' => 'Edit post is success!']);
+				Session::flash('post_status', [
+						'status' => 'success',
+						'message' => 'Edit post is success!']);
 				return redirect()->back();
 			}
-		}
-		else
+		} else
 			return redirect('post/edit');
 	}
 
 	/**
 	 * Update the specified resource in storage.
-	 *
-	 * @param  int  $id
+	 * 
+	 * @param int $id        	
 	 * @return Response
 	 */
-	public function update($id)
+	public function update ($id)
 	{
 		//
 	}
 
 	/**
 	 * Remove the specified resource from storage.
-	 *
-	 * @param  int  $id
+	 * 
+	 * @param int $id        	
 	 * @return Response
 	 */
-	public function deleteDelete()
+	public function deleteDelete ()
 	{
 		$data = Input::all();
-		$rules = ['post_id' => 'required|exists:posts,id'];
+		$rules = [
+				'post_id' => 'required|exists:posts,id'];
 		$validator = Validator::make($data, $rules);
-		if($validator->fails())
+		if ($validator->fails())
 			return 404;
-		else{
+		else {
 			$check_role = Post::whereUser_id(Auth::user()->id)->whereId($data['post_id'])->first();
 			$check_delete_post = Post::whereDelete_status(0)->whereId($data['post_id'])->first();
-			if($check_role && $check_delete_post){
+			if ($check_role && $check_delete_post) {
 				$check_delete_post->delete_status = 1;
 				$check_delete_post->save();
 				return 200;
-			}
-			else
+			} else
 				return 404;
 		}
 	}
-
 }
--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -1,5 +1,6 @@
-<?php namespace App\Http\Controllers;
+<?php
 
+namespace App\Http\Controllers;
 use App\Http\Requests;
 use App\Http\Controllers\Controller;
 use Auth;
@@ -16,99 +17,115 @@ use Mail;
 use URL;
 use App\Post;
 use App\Category;
-class UserController extends Controller {
 
+class UserController extends Controller {
 
 	/**
 	 * Create a new controller instance.
-	 *
+	 * 
 	 * @return void
 	 */
-	public function __construct()
+	public function __construct ()
 	{
-		$this->middleware('auth', ['only' => 'getProfile']);
-		$this->middleware('admin', ['only' => ['getCreate', 'postCreate']]);
+		$this->middleware('auth', [
+				'only' => 'getProfile']);
+		$this->middleware('admin', [
+				'only' => [
+						'getCreate',
+						'postCreate']]);
 	}
 
 	/**
 	 * get Login function
+	 * 
 	 * @author Tran Van Moi <[moitran92@gmail.com]>
-	 * @since  2015/05/12
+	 * @since 2015/05/12
 	 * @return Response
 	 */
-	public function getLogin(){
-		/* Delete all session
-			Session::flush(); */
-		if(Auth::check())
+	public function getLogin ()
+	{
+		/*
+		 * Delete all session
+		 * Session::flush();
+		 */
+		if (Auth::check())
 			return redirect('/');
 		return view('user/login');
 	}
-	
+
 	/**
 	 * post Login function
+	 * 
 	 * @author Tran Van Moi <[moitran92@gmail.com]>
-	 * @since  2015/05/15
+	 * @since 2015/05/15
 	 * @return Response
 	 */
-	public function postLogin(){
+	public function postLogin ()
+	{
 		$data = Input::all();
 		$validator = User::validate($data);
-		if ($validator->fails()){
-	      	// If validation failed redirect back to login.
-	      	return Redirect::to('user/login')->withInput()->withErrors($validator);
-	    }
-	    else{
-	    	$userdata = array(
-			    'email' => Input::get('email'),
-			    'password' => Input::get('password')
-		    );
-		    /* 
-		    	login by email or name
-		    	$field = filter_var($usernameinput, FILTER_VALIDATE_EMAIL) ? 'email' : 'name';
-				if (Auth::attempt(array($field => $usernameinput, 'password' => $password))){
-			*/
-			if (Auth::attempt($userdata)){
-			    return redirect('/');
-			}
-			else {
-				// Session::flash('error', 'Username or password is not correct');
+		if ($validator->fails()) {
+			// If validation failed redirect back to login.
+			return Redirect::to('user/login')->withInput()->withErrors($validator);
+		} else {
+			$userdata = array(
+					'email' => Input::get('email'),
+					'password' => Input::get('password'));
+			/*
+			 * login by email or name
+			 * $field = filter_var($usernameinput, FILTER_VALIDATE_EMAIL) ?
+			 * 'email' : 'name';
+			 * if (Auth::attempt(array($field => $usernameinput, 'password' =>
+			 * $password))){
+			 */
+			if (Auth::attempt($userdata)) {
+				return redirect('/');
+			} else {
+				// Session::flash('error', 'Username or password is not
+				// correct');
 				return redirect('user/login')->withInput()->with('error', 'Username or password is not correct');
 			}
-	    }
+		}
 	}
 
 	/**
 	 * Facebook redirect function
+	 * 
 	 * @author Tran Van Moi <[moitran92@gmail.com]>
-	 * @since  2015/05/13
+	 * @since 2015/05/13
 	 * @return redirect to login
 	 */
-  	public function getFacebookredirect(){
-    	return Socialize::with('facebook')->redirect();
-  	}
+	public function getFacebookredirect ()
+	{
+		return Socialize::with('facebook')->redirect();
+	}
 
-  	/**
+	/**
 	 * Google redirect function
+	 * 
 	 * @author Tran Van Moi <[moitran92@gmail.com]>
-	 * @since  2015/05/13
+	 * @since 2015/05/13
 	 * @return redirect to login
 	 */
-  	public function getGoogleredirect(){
-    	return Socialize::with('google')->redirect();
-  	}
+	public function getGoogleredirect ()
+	{
+		return Socialize::with('google')->redirect();
+	}
 
 	/**
 	 * Login with facebook function
+	 * 
 	 * @author Tran Van Moi <[moitran92@gmail.com]>
-	 * @since  2015/05/12
+	 * @since 2015/05/12
 	 * @return Response
 	 */
-	public function getLoginfb(){
+	public function getLoginfb ()
+	{
 		try {
 			$user_fb = Socialize::with('facebook')->user();
 			$user = User::whereEmail($user_fb['email'])->first();
 			// Create account, login and send new account to user
-			if(!$user){
+			if (! $user) {
 				$user_fb['image'] = $user_fb->avatar;
 				$user = User::create_user($user_fb, env('USER'));
 			}
@@ -117,30 +134,30 @@ class UserController extends Controller {
 			$user->image = $user_fb->avatar;
 			$user->save();
 			return redirect('/');
-			
-		} catch(Exception $e){
+		} catch (Exception $e) {
 			return redirect('user/login')->withInput()->with('error', 'Login with facebook failed! Please try again!');
 		}
 	}
 
 	/**
 	 * Login with google function
+	 * 
 	 * @author Tran Van Moi <[moitran92@gmail.com]>
-	 * @since  2015/05/12
+	 * @since 2015/05/12
 	 * @return Response
 	 */
-	public function getLogingg(){
+	public function getLogingg ()
+	{
 		try {
 			$user_gg = Socialize::with('google')->user();
 			$user = User::whereEmail($user_gg->email)->first();
 			// Create account, login and send new account to user
-			if(!$user){
+			if (! $user) {
 				$user_data = [
-					'id' => $user_gg->id,
-					'name' => $user_gg->name,
-					'email' => $user_gg->email,
-					'image' => $user_gg->avatar
-				];
+						'id' => $user_gg->id,
+						'name' => $user_gg->name,
+						'email' => $user_gg->email,
+						'image' => $user_gg->avatar];
 				$user = User::create_user($user_data, env('USER'));
 			}
 			// Create session
@@ -148,101 +165,114 @@ class UserController extends Controller {
 			$user->image = $user_gg->avatar;
 			$user->save();
 			return redirect('/');
-			
-		} catch(Exception $e){
+		} catch (Exception $e) {
 			return redirect('user/login')->withInput()->with('error', 'Login with google failed! Please try again!');
 		}
 	}
 
 	/**
 	 * Register function
+	 * 
 	 * @author Tran Van Moi <[moitran92@gmail.com]>
-	 * @since  2015/05/12
+	 * @since 2015/05/12
 	 * @return Response
 	 */
-	public function getRegister()
+	public function getRegister ()
 	{
 		return view('user/register');
 	}
 
 	/**
 	 * post Register function
+	 * 
 	 * @author Tran Van Moi <[moitran92@gmail.com]>
-	 * @since  2015/05/12
+	 * @since 2015/05/12
 	 * @return Response
 	 */
-	public function postRegister(){
+	public function postRegister ()
+	{
 		$data = Input::all();
 		$validator = User::validate($data, env('FACTORY_REGISTER'));
-		if ($validator->fails()){
-	      	// If validation failed redirect back to login.
-	      	return redirect('user/register')->withInput()->withErrors($validator);
-	    }
-	    else{
-	    	$userdata = array(
-	    		'name' => Input::get('name'),
-			    'email' => Input::get('email'),
-			    'password' => Input::get('password'),
-			    'image' => 'default.jpg',
-		    );
-		    $role_name = Input::get('is_owner') === 'on' ? env('OWNER') : env('USER');
-		    $user = User::create_user($userdata, $role_name);
-		    if(count($user)){
-				return redirect('user/register')->withInput()->with('register_status', ['status' => 'success', 'message' => 'Register user is success, You can login now!']);
-		    }
-			else
-				return redirect('user/register')->withInput()->with('register_status', ['status' => 'danger', 'message' => 'Register user is failed, Please try again!']);
-	    }
+		if ($validator->fails()) {
+			// If validation failed redirect back to login.
+			return redirect('user/register')->withInput()->withErrors($validator);
+		} else {
+			$userdata = array(
+					'name' => Input::get('name'),
+					'email' => Input::get('email'),
+					'password' => Input::get('password'),
+					'image' => 'default.jpg');
+			$role_name = Input::get('is_owner') === 'on' ? env('OWNER') : env('USER');
+			$user = User::create_user($userdata, $role_name);
+			if (count($user)) {
+				return redirect('user/register')->withInput()->with('register_status', 
+						[
+								'status' => 'success',
+								'message' => 'Register user is success, You can login now!']);
+			} else
+				return redirect('user/register')->withInput()->with('register_status', 
+						[
+								'status' => 'danger',
+								'message' => 'Register user is failed, Please try again!']);
+		}
 	}
 
 	/**
 	 * Logout function
+	 * 
 	 * @author Tran Van Moi <[moitran92@gmail.com]>
-	 * @since  2015/05/12
+	 * @since 2015/05/12
 	 * @return Response
 	 */
-	public function getLogout(){
+	public function getLogout ()
+	{
 		Auth::logout();
 		return redirect('home');
 	}
 
 	/**
 	 * Profile function
+	 * 
 	 * @author Tran Van Moi <[moitran92@gmail.com]>
-	 * @since  2015/05/12
+	 * @since 2015/05/12
 	 * @return Response
 	 */
-	public function getProfile($category_id = 0){
+	public function getProfile ($category_id = 0)
+	{
 		$category_id = trim($category_id);
 		$url_image = "";
-		if(filter_var(Auth::user()->image, FILTER_VALIDATE_URL) === false)
+		if (filter_var(Auth::user()->image, FILTER_VALIDATE_URL) === false)
 			$url_image = URL::to('/') . "/public/images/avatar/" . Auth::user()->image;
 		else
 			$url_image = Auth::user()->image;
 		Session::put('url_image_auth', $url_image);
 		$posts = Post::get_all_posts($category_id, true);
-        $data['posts'] = $posts;
-        $data['categories'] = Category::all();
+		$data['posts'] = $posts;
+		$data['categories'] = Category::all();
 		return view('user/profile', $data);
 	}
 
-  	public function getForgot(){
-  		$hashKey = env('APP_KEY');
-  		if (\Request::isMethod('post')){
-	  		$data['token'] = hash_hmac('sha256', str_random(40), $hashKey);
-	  		Mail::send('emails.password', $data, function($message)
-			{
-			    $message->to('moitran92@gmail.com', 'John Smith')->subject('Welcome!');
-			});
-  		}
-  		return view('user/forgot');
-  	}
+	public function getForgot ()
+	{
+		$hashKey = env('APP_KEY');
+		if (\Request::isMethod('post')) {
+			$data['token'] = hash_hmac('sha256', str_random(40), $hashKey);
+			Mail::send('emails.password', $data, 
+					function  ($message)
+					{
+						$message->to('moitran92@gmail.com', 'John Smith')->subject('Welcome!');
+					});
+		}
+		return view('user/forgot');
+	}
 
-  	public function getCreate(){
-  		echo "create user";
-  	}
+	public function getCreate ()
+	{
+		echo "create user";
+	}
 
-  	public function postCreate(){
-  		echo "ok";
-  	}
+	public function postCreate ()
+	{
+		echo "ok";
+	}
 }
